4e344e23a60f361255d174fabb2420094bc1c644
Ariana Chae <chae.ariana@gmail.com>
2013-04-17 22:07:32 -0400

    Updating Bluetooth

--- src/com/android/gesture/builder/POEBluetoothAdaptor.java
+++ src/com/android/gesture/builder/POEBluetoothAdaptor.java
@@ -18,6 +18,8 @@ public class POEBluetoothAdaptor {
 	String mStatus;
 	String connection_status;
 	String bluetooth_name;
+	String Socket_connection_status;
+	String Sending_status;
 	BluetoothDevice connected_device;
 	BluetoothSocket mSocket;
 	private InputStream mInputStream;
@@ -68,25 +70,77 @@ public class POEBluetoothAdaptor {
 		// 3. Initiate pairing
 	}
 	
-	public void send(int[] toArduino) throws IOException {
+	public void send(int[] toArduino) throws IOException 
+	{
+		
 		UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); //Standard SerialPortService ID
-        if (connected_device != null) {
-        	
-        }
+		/*
+		if (Build.VERSION.SDK_INT >= 10){
+			try {
+				final Method  m = connected_device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", new Class[] { UUID.class });
+				mSocket = (BluetoothSocket) m.invoke(connected_device, uuid);
+	          } catch (Exception e) {
+	  			mSocket = connected_device.createRfcommSocketToServiceRecord(uuid);
+	          }
+			
+		}
+		else {
+			mSocket = connected_device.createRfcommSocketToServiceRecord(uuid);
+		}
+		*/
 		mSocket = connected_device.createRfcommSocketToServiceRecord(uuid);
-        mSocket.connect();
+		try {
+            // Connect the device through the socket. This will block
+            // until it succeeds or throws an exception
+            mSocket.connect();
+        	Socket_connection_status = "Sending made";
+
+        } catch (IOException connectException) {
+            // Unable to connect; close the socket and get out
+        	Socket_connection_status = "Sending not made";
+          /*
+        	try {
+                mSocket.close();
+            	Socket_connection_status = "Sending was not closed";	
+            } catch (IOException closeException) { }
+            //return;*/
+        }
+		try
+		{
+        	mSocket.connect();
+        	Socket_connection_status = "Sending made";
+        } catch (Exception e){
+        	//Socket_connection_status = "Sending is not made";
+        	Log.d("bluetooth", "...Error data send: " + e.getMessage() + "...");   
+        }
+        
         mOutputStream = mSocket.getOutputStream();
-        //mInputStream = mSocket.getInputStream();
+        mInputStream = mSocket.getInputStream();
 		// Send toArduino array to serial port
         byte[] bytetoArduino = new byte[toArduino.length];
         for (int i = 0; i < toArduino.length; i++) {
         	bytetoArduino[i] = (byte) toArduino[i];
         }
+        bluetooth.cancelDiscovery();
+        //Socket_connection_status = "wow";
+        //mOutputStream.write(2);
         try {
-        	mOutputStream.write(bytetoArduino);
-        } catch (IOException e) {
-            Log.d("bluetooth", "...Error data send: " + e.getMessage() + "...");     
-          }
+        	mOutputStream.write(bytetoArduino,0,bytetoArduino.length);
+        	Socket_connection_status = "wow";
+        }
         
+        catch (IOException e) 
+        {
+        	Socket_connection_status = "T.T";
+            Log.d("bluetooth", "...Error data send: " + e.getMessage() + "...");     
+        }
+        closeBT();
+    }
+	public void closeBT() throws IOException
+    {
+        mOutputStream.close();
+        mInputStream.close();
+        mSocket.close();
 	}
 }
+
